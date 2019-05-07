714445659d47aaa852f91862d1a01ceb0c71e2f0
Jade Lacosse <jade.lacosse@stocksy.com>
2019-05-07 00:24:33 -0700

    Config system now works with freshly programmed arduino

--- meter-arduino/meter-arduino.ino
+++ meter-arduino/meter-arduino.ino
@@ -14,21 +14,24 @@
 */
 
 /*
-  Simple 3-Gauge Meter
+  Multi-Gauge Analog Meter
   Author: Jade Lacosse
  */
-#define METERS 1
+#define PINS 12
 #define ONE_SECOND  1000000
 #define SLEEP_DELAY 2000000
 #define ATTACK      125000
 #define RELEASE     2000000
-int meterPins[1] =  {11};
-int targetValues[METERS];
-int currentValues[METERS];
-int meterVelocity[METERS];
+int VALID_OUTPUTS[] = {0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1}; // Pins 3, 5, 6, 9, 10 , and 11 allow PWM output.
+int pinState[PINS]; // when first writing to a pin, we set it to OUTPUT
+int targetValues[PINS];
+int currentValues[PINS];
+int meterVelocity[PINS];
 int charsReceived = 0;
+int lastChar = 0;
+int currentPin = 0;
 
-unsigned long nextTick[METERS];
+unsigned long nextTick[PINS];
 unsigned long messageDelay = ONE_SECOND; // 1 second to start
 unsigned long currentTime = micros();
 unsigned long lastMessageTime = currentTime;
@@ -42,8 +45,10 @@ bool dataReady = false;
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  for (int meter=0; meter < METERS; meter++) {
-    pinMode(meterPins[meter], OUTPUT);
+  for (int pin=0; pin < PINS; pin++) {
+    if (VALID_OUTPUTS[pin] == 1) {
+      pinMode(pin, OUTPUT);
+    }
   }
 }
 
@@ -54,9 +59,11 @@ void setup() {
 void loop() {
   currentTime = micros();
   if (dataReady) {
-    for (int meter = 0;meter < METERS; meter++) {
-      if (currentTime >= nextTick[meter]) {
-        updatePin(meter);
+    for (int pin = 0;pin < PINS; pin++) {
+      if (pinState[pin] == 1) { // If the pin is turned on, check to see if it's updated.
+        if (currentTime >= nextTick[pin]) {
+          updatePin(pin);
+        }
       }
     }
   }
@@ -69,13 +76,12 @@ void loop() {
  * Bring the gauges down to zero, prepare the meter for its next message.
  */
 void sleep() {
-  for (int meter=0; meter < METERS; meter++) {
+  for (int meter=0; meter < PINS; meter++) {
     targetValues[meter] = 0;
   }
   charsReceived = 0;
   lastMessageTime = currentTime;
   targetTime = currentTime + SLEEP_DELAY;
-  Serial.write(42);
 }
 
 /**
@@ -84,7 +90,7 @@ void sleep() {
 void updatePin(int meter) {
   updateScheduleForMeter(meter);
   currentValues[meter] = constrain(currentValues[meter] + meterVelocity[meter], 0, 255); 
-  analogWrite(meterPins[meter], currentValues[meter]);
+  analogWrite(meter, currentValues[meter]);
 }
 
 /**
@@ -96,8 +102,8 @@ void updateScheduleForMeter(int meter) {
   int deltaValue = abs(targetValue - currentValue);
   unsigned long remainingTime = max(targetTime - currentTime, (targetValue > currentValue) ? ATTACK : RELEASE); // currentTime global is updated in the loop.
   if (deltaValue) {
-      nextTick[meter] = (remainingTime/abs(deltaValue)) + currentTime;
-      meterVelocity[meter] = (targetValue > currentValue) ? 1 : -1;
+    nextTick[meter] = (remainingTime/abs(deltaValue)) + currentTime;
+    meterVelocity[meter] = (targetValue > currentValue) ? 1 : -1;
   } else {
     nextTick[meter]= currentTime + remainingTime;
     meterVelocity[meter] = 0;
@@ -114,22 +120,34 @@ void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
     int inChar = (int) Serial.read();
-    targetValues[charsReceived] = inChar;
-    charsReceived++;
-    if (charsReceived == METERS) {
+    if (inChar == 255 && lastChar == 255) {
+      Serial.write(255);
+      charsReceived = 0;
       currentTime = micros();
       messageDelay = min(ONE_SECOND, max(currentTime - lastMessageTime, 10000)); // Limit to 1-1000hz.
       lastMessageTime = currentTime;
       targetTime = currentTime + messageDelay;
-      charsReceived = 0;
       if (!dataReady) {
         dataReady = true;
       }
-      for (int meter=0; meter < METERS; meter++) {
-        updateScheduleForMeter(meter);
+      for (int meter=0; meter < PINS; meter++) {
+        if (VALID_OUTPUTS[meter] == 1) {
+          updateScheduleForMeter(meter);
+        }
+      }
+    } else {
+      if (charsReceived == 0) {
+        currentPin = inChar;
+        charsReceived++;
+
+      } else {
+        targetValues[currentPin] = inChar;
+        pinState[currentPin] = 1;
+        charsReceived = 0;
+        //Serial.write(currentPin);
+        //Serial.write(inChar);
       }
     }
+    lastChar = inChar;
   }
 }
-
-
